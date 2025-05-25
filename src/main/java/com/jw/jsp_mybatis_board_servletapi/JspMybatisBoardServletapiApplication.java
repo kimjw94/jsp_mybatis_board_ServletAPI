c66148128c7b4d7222e978013c5f988b05cb3103
kimjw94 <152935868+kimjw94@users.noreply.github.com>
2025-05-25 14:13:14 +0900

    Create Projec jsp_mybatis_board_ServletAPI(Java8,SpringBoot2.7.18,ojdbc8)

---
+++ src/main/java/com/jw/jsp_mybatis_board_servletapi/JspMybatisBoardServletapiApplication.java
@@ -0,0 +1,13 @@
+package com.jw.jsp_mybatis_board_servletapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspMybatisBoardServletapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspMybatisBoardServletapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jw/jsp_mybatis_board_servletapi/JspMybatisBoardServletapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.jsp_mybatis_board_servletapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspMybatisBoardServletapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
