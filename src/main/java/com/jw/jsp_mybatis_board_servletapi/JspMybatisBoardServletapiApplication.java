dbea4da4dfd8e426575c6e54dcb6aa016ed1f8ac
kimjw94 <152935868+kimjw94@users.noreply.github.com>
2025-06-08 13:33:47 +0900

    Project Crud Test

--- src/main/java/com/jw/jsp_mybatis_board_servletapi/JspMybatisBoardServletapiApplication.java
+++ src/main/java/com/jw/jsp_mybatis_board_servletapi/JspMybatisBoardServletapiApplication.java
@@ -1,7 +1,14 @@
 package com.jw.jsp_mybatis_board_servletapi;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class JspMybatisBoardServletapiApplication {
@@ -10,4 +17,18 @@ public class JspMybatisBoardServletapiApplication {
         SpringApplication.run(JspMybatisBoardServletapiApplication.class, args);
     }
 
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setMapperLocations(
+                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml")
+        );
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
 }
---
+++ src/main/java/com/jw/jsp_mybatis_board_servletapi/dto/TestDTO.java
@@ -0,0 +1,22 @@
+package com.jw.jsp_mybatis_board_servletapi.dto;
+
+public class TestDTO {
+    Long test_id;
+    String test_name;
+
+    public Long getTest_id() {
+        return test_id;
+    }
+
+    public void setTest_id(Long test_id) {
+        this.test_id = test_id;
+    }
+
+    public String getTest_name() {
+        return test_name;
+    }
+
+    public void setTest_name(String test_name) {
+        this.test_name = test_name;
+    }
+}
